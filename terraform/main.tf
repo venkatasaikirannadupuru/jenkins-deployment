cb4d34953e06d4268f2b6ec50a58584d48a1fe86
saikiran <sn317600@gmail.com>
2026-09-15 14:19:02 +0530

    saiiai

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,5 +89,5 @@ resource "aws_lambda_permission" "api_gateway" {
   function_name = aws_lambda_function.employee_management.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.employee_api.execution_arn}/*/*
+  source_arn = "${aws_apigatewayv2_api.employee_api.execution_arn}/*/*"
 }
