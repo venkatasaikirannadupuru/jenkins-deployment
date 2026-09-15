c1a3328c09c60e3e4a892a9995c8f9d66b93f627
saikiran <sn317600@gmail.com>
2026-09-15 13:40:27 +0530

    update s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "employee-management-terraform-state-495234635542"
+    key    = "employee-management/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider "aws" {
