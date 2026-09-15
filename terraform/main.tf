6b5ec83eff6457715be2e1312955bce7b4adfdd2
saikiran <sn317600@gmail.com>
2026-09-15 14:08:21 +0530

    saikiran

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,3 +58,41 @@ resource "aws_lambda_function" "employee_management" {
     aws_iam_role_policy_attachment.lambda_basic_execution
   ]
 }
+resource "aws_apigatewayv2_api" "employee_api" {
+  name          = "employee-management-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.employee_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.employee_management.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "employee_route" {
+  api_id    = aws_apigatewayv2_api.employee_api.id
+  route_key = "GET /employees"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.employee_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.employee_management.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.employee_api.execution_arn}/*/*"
+}
+
+output "api_url" {
+  description = "Employee Management API URL"
+  value       = "${aws_apigatewayv2_api.employee_api.api_endpoint}/employees"
+}
